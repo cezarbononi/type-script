71ee071512e3202bae02cf47f4b65111f913138d
Cezar Bononi <cezarbononi@gmail.com>
2021-12-14 11:38:00 -0300

    adicionando a funcao de add negociacao e de limpar formulario

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,22 +1,35 @@
 import { Negociacao } from "../models/negociacao.js";
 
 export class NegociacaoController {
-    private inputData;
-    private inputQuantidade;
-    private inputValor;
+  private inputData: HTMLInputElement;
+  private inputQuantidade: HTMLInputElement;
+  private inputValor: HTMLInputElement;
 
-    constructor() {
-        this.inputData = document.querySelector('#data');
-        this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputValor = document.querySelector('#valor');
-    }
+  constructor() {
+    this.inputData = document.querySelector("#data");
+    this.inputQuantidade = document.querySelector("#quantidade");
+    this.inputValor = document.querySelector("#valor");
+  }
 
-    adiciona() {
-        const negociacao = new Negociacao(
-            this.inputData.value,
-            this.inputQuantidade.value,
-            this.inputValor.value
-        );
-        console.log(negociacao);
-    }
+  adiciona():void {
+      const negociacao = this.criaNegociacao();
+      console.log(negociacao);
+      this.limparFormulario();
+  }
+
+  criaNegociacao():Negociacao {
+    const exp = /-/g;
+
+    const data = new Date(this.inputData.value.replace(exp, ","));
+    const quantidade = parseInt(this.inputQuantidade.value);
+    const valor = parseFloat(this.inputValor.value);
+    return new Negociacao(data, quantidade, valor);
+  }
+
+  limparFormulario(): void {
+    this.inputData.value = '';
+    this.inputQuantidade.value = '';
+    this.inputValor.value = '';
+    this.inputData.focus();
+  }
 }
