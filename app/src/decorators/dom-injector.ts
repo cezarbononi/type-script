e258b27ed1f7b044f40cad348d4d6f76d230d8ca
Cezar Bononi <cezarbononi@gmail.com>
2022-01-26 14:45:01 -0300

    adding api imports and interfaces

--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -3,10 +3,15 @@ export function domInjector(seletor: string) {
 
         console.log(`Modificando prototype ${target.constructor.name}
         e adicionando getter para a propriedade ${propertyKey}`);
+
+        let elemento: HTMLElement | null = null;
         const getter = function() {
-            const elemento = document.querySelector(seletor);
-            console.log(`buscando elemento do dom com o ${seletor} para injetar em 
-            ${propertyKey}`);
+            if (!elemento) {
+                elemento = <HTMLElement>document.querySelector(seletor); 
+                console.log(`buscando elemento do dom com o ${seletor} para injetar em 
+                ${propertyKey}`);
+            }
+          
             return elemento;
         }
 
