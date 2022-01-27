bea84d1dc2991231edf3325ddf2dbcd81834aa06
Cezar Bononi <cezarbononi@gmail.com>
2022-01-27 16:27:20 -0300

    adding imprimivel interface and implement then

--- app/dist/js/models/negociacao.js
+++ app/dist/js/models/negociacao.js
@@ -7,13 +7,20 @@ export class Negociacao {
     get volume() {
         return this.quantidade * this.valor;
     }
+    paraTexto() {
+        return `
+        Data: ${this.data},
+        Quantidade: ${this.quantidade},
+        Valor: ${this.valor}
+    `;
+    }
     get data() {
         const data = new Date(this._data.getTime());
         return data;
     }
     static criaDe(dataString, quantidadeString, valorString) {
         const exp = /-/g;
-        const date = new Date(dataString.replace(exp, ','));
+        const date = new Date(dataString.replace(exp, ","));
         const quantidade = parseInt(quantidadeString);
         const valor = parseFloat(valorString);
         return new Negociacao(date, quantidade, valor);
